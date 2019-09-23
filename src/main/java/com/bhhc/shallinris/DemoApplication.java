145031e41763338aa51d0c63a55fee9867fa0f66
shallinris <shallin@gmail.com>
2019-09-23 13:42:48 -0700

    fixed type in text

--- src/main/java/com/bhhc/shallinris/DemoApplication.java
+++ src/main/java/com/bhhc/shallinris/DemoApplication.java
@@ -27,8 +27,8 @@ public class DemoApplication {
      */
     @PostConstruct
     public void init() {
-        answerRep.save(new Answer(1, "First, I deeply respect and appreciate Warren Buffet’s investing philosophy and philanthropic work. I believe the philosophy of a company’s leader permeates the company, and as an employee, I would feel pride in my work knowing the overarching principles at any subsidiary of Warren Buffet’s would be non-predatory, and embody integrity, professionalism and sound ethics. "));
+        answerRep.save(new Answer(1, "First, I deeply respect and appreciate Warren Buffett’s investing philosophy and philanthropic work. I believe the philosophy of a company’s leader permeates the company, and as an employee, I would feel pride in my work knowing the overarching principles at any subsidiary of Warren Buffett’s would be non-predatory, and embody integrity, professionalism and sound ethics. "));
         answerRep.save(new Answer(2, "Second, I want to work at a company that is respected because it does its job well. In its chosen markets, BHHC has been awarded the highest rating for insurance carriers by A.M.Best. "));
-        answerRep.save(new Answer(3, "Finally, studies show, and I know from personal experience, that employees are more loyal to and work harder for their company when they are well-treated and taken care of. According to Glassdoor and other reviewers,  BHHC takes great care of their employees in terms of benefits and employee support. They also treat their employees well because they understand the importance of this in retaining good people. "));
+        answerRep.save(new Answer(3, "Finally, studies show, and I know from personal experience, that employees are more loyal to and work harder for their company when they are well-treated. According to Glassdoor and other reviewers,  BHHC takes great care of their employees in terms of benefits and employee support. They also treat their employees well because they understand the importance of this in retaining good people. "));
     }
 }
